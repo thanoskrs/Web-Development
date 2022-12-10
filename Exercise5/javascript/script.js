4ef4199932ea42a4884e79cde228748ce7b43d5f
thanoskrs <thanoskouros99@gmail.com>
2022-12-10 17:55:52 +0200

    add some constraint validations in javascript file

--- Exercise5/javascript/script.js
+++ Exercise5/javascript/script.js
@@ -23,6 +23,7 @@ var check_password = function() {
 
   function email_check() {
     const input = document.getElementById("txt_email");
+    console.log(input.value)
     if (!input.checkValidity()) {
       document.getElementById("email_error").innerHTML = input.validationMessage;
       document.getElementById('email_error').style.color = 'red';
@@ -44,3 +45,43 @@ var check_password = function() {
         document.getElementById('phone_error').style.color = 'green';
       } 
   }
+
+window.onload = function() {
+
+  let birthdayDateElement = document.getElementById("birthday_date")
+  
+  birthdayDateElement.onchange = function() {
+    let birthdayDate = new Date(birthdayDateElement.value)
+    let today = new Date()
+    let valid = (today.getFullYear() - birthdayDate.getFullYear()) >= 18
+
+    if (!valid) {
+      birthdayDateElement.setCustomValidity("Your age should be at least 18 years.")
+    } else {
+      this.setCustomValidity("")
+    }
+  }
+
+
+  let passwordElement = document.getElementById("txt_password")
+  passwordElement.onchange = function() {
+    let passwordText = document.getElementById("txt_password").value
+
+    const specialCharacters = /[!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/
+    const numbers = /[0-9]/
+    const upperCaseLetters = /[A-Z]/
+
+    if (!specialCharacters.test(passwordText)) {
+      document.getElementById("txt_password").setCustomValidity("The password should contain at least one special character.")
+    }  else if (!numbers.test(passwordText)) {
+      document.getElementById("txt_password").setCustomValidity("Password should contain at least one number.")
+    } else if (!upperCaseLetters.test(passwordText)) {
+      document.getElementById("txt_password").setCustomValidity("Password should contain at least one upper case letter.")
+    } else {
+      this.setCustomValidity("")
+    }
+  }
+}
+
+
+
